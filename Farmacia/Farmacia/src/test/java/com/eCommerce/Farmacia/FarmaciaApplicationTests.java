87fc12225a903a961a029a665e9472afd20edd46
Karolyne Corol <karolyne.corol@gmail.com>
2022-01-25 10:55:05 -0300

    @KarolyneCorol [First commit]

---
+++ Farmacia/Farmacia/src/test/java/com/eCommerce/Farmacia/FarmaciaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eCommerce.Farmacia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FarmaciaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
